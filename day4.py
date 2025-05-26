00dffb017e20bcd7167dc6e8b7b402718c65eda3
Sousa <kcsfeitosa@gmail.com>
2025-05-26 12:18:45 -0300

    cont. aula

--- day4.py
+++ day4.py
@@ -13,3 +13,8 @@ print("Exponentiation (10**3):", num1 ** num2)
 print("\n--- Relational Operations ---")
 print("Is 10 equal to 3? ", num1 == num2)
 print("Is 10 not equal to 3? ", num1 != num2)
+print("Is 10 greater than 3? ", num1 > num2)
+print("Is 10 less than 3? ", num1 < num2)
+print("Is 10 greater than or equal to 3? ", num1 >= num2)
+print("Is 10 less than or equal to 3? ", num1 <= num2)
+

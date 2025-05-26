186290fdaeb19637747efff28b0f768fadfc8e90
ks <kcsfeitosa@gmail.com>
2025-05-26 09:11:36 -0300

    andamento aula

--- day4.py
+++ day4.py
@@ -1,2 +1,15 @@
 ## day4 - Basic Operator and Expressions
 
+num1 = 10
+num2 = 3
+print("\n--- Arithmwtic Operations ---")
+print("Addition (10+3):", num1 + num2)
+print("Subtraction (10-3):", num1 - num2)
+print("Multiplication (10*3):", num1 * num2)
+print("Division (10/3):", num1 / num2)
+print("Floor Division (10//3)", num1 // num2)
+print("Exponentiation (10**3):", num1 ** num2)
+
+print("\n--- Relational Operations ---")
+print("Is 10 equal to 3? ", num1 == num2)
+print("Is 10 not equal to 3? ", num1 != num2)

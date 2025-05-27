b66546698a1a641edcb1c2eae96e0496189f2037
ks <kcsfeitosa@gmail.com>
2025-05-27 07:47:48 -0300

    cont. aula

--- day5.py
+++ day5.py
@@ -1,1 +1,5 @@
 ## day5 - Conditional Statements and Loops
+###     If-else Statements      ###
+# Program 1: Check if number is evenm/odd
+number = int(input("Enter an interer: "))
+if number % 2 == 0:

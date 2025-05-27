71d6ee9de4b412f0b4fa6cb7c3e7a314c379ee89
Sousa <kcsfeitosa@gmail.com>
2025-05-26 21:42:02 -0300

    cont. aula

--- day4.py
+++ day4.py
@@ -21,3 +21,13 @@ print("Is 10 less than or equal to 3? ", num1 <= num2)
 true_value = True
 false_value = False
 
+print("\n--- Logical Operations ---")
+print("True AND False: ", true_value and false_value)
+print("True AND True: ", true_value and true_value)
+print("False AND False: ", false_value and false_value)
+print("True OR False: ", true_value or false_value)
+print("True OR True: ", true_value or true_value)
+print("False OR False: ", false_value or false_value)
+print("NOT True: ", not true_value)
+print("NOT False: ", not false_value)
+

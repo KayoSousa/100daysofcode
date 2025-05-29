7a823527a3d5ff62d68d235d652b40502ea1cc2a
Sousa <kcsfeitosa@gmail.com>
2025-05-28 21:59:16 -0300

    cont.aula

--- day6.py
+++ day6.py
@@ -10,8 +10,21 @@ greet_user("Kayo")
 print()
 
 #####     Default and Keyword Arguments      #####
+
 # Program 2: Greet_user function with default values as argument
 def greet_user(name="Guest"):
     print(f"Hello,{name}")
 
 greet_user() #Output: Hello, Guest
+greet_user("Kayo") #Output: Hello, Kayo
+print()
+
+#####     Function With Return Values      #####
+
+# Program 3: Calculate and returns the area of a retangle
+def calculate_area(length, breadth):
+    return length * breadth
+
+area = calculate_area(5, 3)
+print(f"The area of the retangle is {area}.")
+print()

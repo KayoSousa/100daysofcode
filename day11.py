da8ada4db2e9bd01fcf2b8a3d0855d7cd803fe7e
ks <kcsfeitosa@gmail.com>
2025-06-03 11:53:43 -0300

    Inicializacao de repositorio

---
+++ day11.py
@@ -0,0 +1,10 @@
+## day11 - While Loop
+
+# 1. Use a While loop to print even numbers from 2 to 20.
+
+number_even = 2
+
+while number_even <= 20:
+    print(number_even)
+    number_even = number_even + 2
+print(f"O loop terminou, tornando assim o número maior que o máximo.")

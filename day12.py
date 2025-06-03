ebb8e4067558ffd02a0517a67afd9395262d360e
ks <kcsfeitosa@gmail.com>
2025-06-03 13:35:09 -0300

    Inicializacao de repositorio

---
+++ day12.py
@@ -0,0 +1,9 @@
+## day12 - odd-even
+
+# 1. write a program to check if a number is even or odd.
+
+number = int(input(f"Enter an number interer: "))
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")

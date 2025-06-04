1a37d1e9784a1a215e26f3a53f87a6aaadc78395
ks <kcsfeitosa@gmail.com>
2025-06-04 13:29:08 -0300

    cont.aula

---
+++ day14.py
@@ -0,0 +1,12 @@
+## day14 - Leap Year
+
+# 1. write a program that checks if a given input year is a leap year or not.
+
+while True:
+    try:
+        year_str = input("Enter a year to see if it is a leap year: ")
+        year = int(year_str)
+        break
+    except ValueError:
+        print(f"Error: '{year_str}' not is a year valid. Please, enter only whole numbers.)
+              

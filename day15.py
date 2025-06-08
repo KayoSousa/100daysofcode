5c1b0d94e97df8bcd3b37805167a6e0832c81336
Sousa <kcsfeitosa@gmail.com>
2025-06-08 13:23:08 -0300

    inicializacao de repositorio

--- day15.py
+++ day15.py
@@ -1,3 +1,11 @@
 ## day15 - Factorial
 
 # 1. write a function to calculate the factorial of a number.
+
+number = int(input("\nEntrer a number to calculate its factorial: "))
+factorial = 1
+
+while number > 1:
+    factorial *= number
+    number -= 1
+print(f"The factorial of {number} is {factorial}.")
